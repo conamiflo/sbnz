183cd14ac2910eb059f44a7b85a3df13c58f6141
OgnjenGligoric <gligoric383@gmail.com>
2025-10-03 23:23:22 +0200

    removed test data

--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/RecommendationController.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/RecommendationController.java
@@ -101,63 +101,7 @@ public class RecommendationController {
         user4.addInterest("food");
         
         request.setUsers(List.of(user1, user2, user3, user4));
-        
-        Post post1 = new Post("post1", "user3", "HIIT Summer Workout - 15 minuta do savršene forme!",
-                             "video", "fitness");
-        post1.addHashtag("#fitness");
-        post1.addHashtag("#hiit");
-        post1.addHashtag("#summer");
-        post1.setLikes(150);
-        post1.setComments(25);
-        post1.setShares(12);
-        post1.setReach(2500);
-        post1.calculateEngagementRate();
-        
-        Post post2 = new Post("post2", "user2", "Najbolji recept za letnju salatu sa avokadom", 
-                             "image", "food");
-        post2.addHashtag("#healthyfood");
-        post2.addHashtag("#avocado");
-        post2.addHashtag("#summer");
-        post2.setLikes(89);
-        post2.setComments(15);
-        post2.setShares(8);
-        post2.setReach(1800);
-        post2.calculateEngagementRate();
-        
-        Post post3 = new Post("post3", "user4", "Moja jutarnja rutina za produktivan dan", 
-                             "video", "lifestyle");
-        post3.addHashtag("#lifestyle");
-        post3.addHashtag("#morning");
-        post3.addHashtag("#routine");
-        post3.setLikes(95);
-        post3.setComments(18);
-        post3.setShares(6);
-        post3.setReach(2200);
-        post3.calculateEngagementRate();
-        
-        Post post4 = new Post("post4", "user1", "Beach body transformation - pre i posle", 
-                             "image", "fitness");
-        post4.addHashtag("#transformation");
-        post4.addHashtag("#fitness");
-        post4.addHashtag("#beach");
-        post4.setLikes(203);
-        post4.setComments(31);
-        post4.setShares(18);
-        post4.setReach(4200);
-        post4.calculateEngagementRate();
-        
-        Post post5 = new Post("post5", "user4", "Top 5 mesta u Novom Sadu za savršenu fotografiju", 
-                             "text", "lifestyle");
-        post5.addHashtag("#novised");
-        post5.addHashtag("#photography");
-        post5.addHashtag("#lifestyle");
-        post5.setLikes(67);
-        post5.setComments(12);
-        post5.setShares(4);
-        post5.setReach(1500);
-        post5.calculateEngagementRate();
-        
-        request.setPosts(List.of(post1, post2, post3, post4, post5));
+
         
         return request;
     }
