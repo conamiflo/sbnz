bce7f590edaf2480b1bc600ae58ae2f78f8f916d
OgnjenGligoric <gligoric383@gmail.com>
2025-10-14 00:02:09 +0200

    renamed backward file name

--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/controllers/BackwardChainingController.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/controllers/BackwardChainingController.java
@@ -1,10 +1,19 @@
 package com.ftn.sbnz.service.controllers;
 
+import com.ftn.sbnz.model.models.Post;
+import com.ftn.sbnz.model.models.Recommendation;
+import com.ftn.sbnz.model.models.User;
+import com.ftn.sbnz.service.repositories.PostRepository;
 import com.ftn.sbnz.service.repositories.UserRepository;
 import com.ftn.sbnz.service.services.BackwardChainingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/backward")
